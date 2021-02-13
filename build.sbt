1ebee29aca58486a1b789be5cc80011c4557663a
Alexandre Roba <alexandre.roba@arobanet.be>
2021-02-13 20:41:28 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "purecomplete"
+
+version := "0.1"
+
+scalaVersion := "2.13.4"
---
+++ src/main/scala/be/arobanet/purecomplete/PureComplete.scala
@@ -0,0 +1,6 @@
+package be.arobanet.purecomplete
+
+object PureComplete {
+  def main(args: Array[String]): Unit =
+    println("Hello world")
+}
